2d4baaa67fc9367dea25d3b84404cbf8806a15e5
susan-sapkota-98 <susansapkota986@gmail.com>
2026-03-22 23:10:28 +0545

    fix db connection pool

--- utils/db.js
+++ utils/db.js
@@ -2,19 +2,15 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'employeems'
-})
-
-conn.connect(function(err) {
-    if(err) {
-        console.log("Connection error: " + err.message)
-    } else {
-        console.log("Connected")
-    }
+    database: process.env.DB_NAME || 'employeems',
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
 export default conn;
