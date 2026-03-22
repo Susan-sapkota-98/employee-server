49d86ba3530b35e1fb36666bc1b00fb03ecf5ba0
susan-sapkota-98 <susansapkota986@gmail.com>
2026-03-22 23:04:56 +0545

    fix cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://employee-frontend-black.vercel.app"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }))
